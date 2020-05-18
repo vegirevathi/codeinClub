8214b4116329afceab98c3f3d683b2165c5e5ca1
vegirevathi <vegirevathi@gmail.com>
2020-05-18 16:06:21 +0530

    Switch Case is used

---
+++ empWageComputationUC4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+echo "Welcome to Employee Wage Computation"
+Present=1;
+PartTime=2;
+empAttendanceCheck=$(( RANDOM%3 ))
+echo "Employee Attendance Check" $empAttendanceCheck
+wagePerHr=20;
+
+case $empAttendanceCheck in
+$Present)
+	empHrs=8
+	;;
+$PartTime)
+	empHrs=4
+	;;
+*)
+	empHrs=0
+	;;
+esac
+EmployeeWage=$(( $empHrs*$wagePerHr ))
+echo "Employee Wage " $EmployeeWage
