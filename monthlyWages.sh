aafbee41e3b53e11c03c43be6c9efd660b0be7b3
vegirevathi <vegirevathi@gmail.com>
2020-05-18 13:34:04 +0530

    calculating Wages for a month

---
+++ monthlyWages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+numWorkingDays=20;
+for (( day=1; day<=numWorkingDays; day++ ))
+do 
+	 empCheck=$((RANDOM%3));
+                case $empCheck in
+                        $isFullTime)
+                                empHrs=4
+                                ;;
+                        $isPartTime)
+                                empHrs=4
+                                ;;
+                        *)
+                                empHrs=0
+                                ;;
+                        esac
+                salary=$(($empHrs*$empRatePerHr));
+                totalSalary=$(( $totalSalary+$salary));
+done
+
